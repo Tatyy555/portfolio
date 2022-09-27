7b6717045392c0f2a3e7f95e03ff4905810078de
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-09-27 22:06:38 +0900

    ammend project

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -39,50 +39,49 @@ export default function ProjectSection({ projects }: Props) {
           {projects.map((project, i) => (
             <div
               key={project._id}
-              className=" w-full h-full flex-shrink-0 snap-center flex flex-col items-center justify-center   lg:grid lg:grid-cols-10 p-20 lg:space-x-20 "
+              className=" w-full h-full flex-shrink-0 snap-center flex flex-col items-center justify-center   0 p-5 lg:space-x-20 "
             >
-              {/* // Left */}
-              <motion.div
-                initial={{ y: -30, opacity: 0 }}
-                transition={{ duration: 1 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                viewport={{ once: true }}
-                className="justify-center items-center object-center lg:col-span-5  max-w-[400px]"
-              >
-                <div className="rounded-xl bg-mine p-2">
-                  <Image
-                    src={urlFor(project.image).url()}
-                    height={500}
-                    width={700}
-                    objectFit="contain"
-                    className=""
-                  />{" "}
-                </div>
-              </motion.div>
-              {/* Right */}
-              <div
-                className="max-w-[400px] lg:col-span-5 " 
-              >
-                <h4 className="text-3xl md:text-4xl font-semibold text-center lg:space-y-10">
-                  <span className=" underline decoration-yellow-300/50 ">
-                    Case {1 + i} of {projects.length}
-                  </span>{" "}
-                  <h4>
-                  {project?.title}
+              <div className="border-4 border-my-skyblue p-5 h-[99%] w-[90%] flex flex-col items-center lg:grid lg:grid-cols-10 lg:flex-row ">
+                {/* // Left */}
+                <motion.div
+                  initial={{ y: -30, opacity: 0 }}
+                  transition={{ duration: 1 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  viewport={{ once: true }}
+                  className="justify-center items-center object-center lg:col-span-5 lg:mx-auto  max-w-[400px]"
+                >
+                  <div className="rounded-xl bg-mine p-2">
+                    <Image
+                      src={urlFor(project.image).url()}
+                      height={500}
+                      width={700}
+                      objectFit="contain"
+                      className=""
+                    />{" "}
+                  </div>
+                </motion.div>
+                {/* Right */}
+                <div className="max-w-[450px] lg:col-span-5 lg:mx-auto">
+                  <h4 className="text-3xl md:text-4xl font-semibold text-center lg:space-y-10">
+                    <span className=" underline decoration-yellow-300/50 ">
+                      Case {1 + i} of {projects.length}
+                    </span>{" "}
+                    <h4>{project?.title}</h4>
                   </h4>
-                </h4>
-                <div className="flex items-center space-x-2 justify-center mt-5 lg:mt-10 lg:mb-5">
-                  {project?.technologies.map((technology) => (
-                    <img
-                      className="h-10 w-10 object-cover rounded-full"
-                      key={technology._id}
-                      src={urlFor(technology.image).url()}
-                    />
-                  ))}
+                  <div className="flex items-center justify-center mt-4 mb-2 lg:mt-10 lg:mb-5 w-fit space-x-2 mx-auto">
+                    {project?.technologies.map((technology) => (
+                      <div key={technology._id}>
+                        <Image
+                          height={40}
+                          width={40}
+                          className="object-cover rounded-full"
+                          src={urlFor(technology.image).url()}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <p className="text-lg text-center">{project?.summary}</p>
                 </div>
-                <p className="text-lg text-center">
-                  {project?.summary}
-                </p>
               </div>
             </div>
           ))}
