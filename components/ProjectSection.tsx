e15b566bb0e593ddd28a70e95f4b7600e47d8a81
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-09-27 22:49:32 +0900

    modify

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -38,7 +38,7 @@ export default function ProjectSection({ projects }: Props) {
         <div className="bg-my-skyblue/30 shadow-xl shadow-blue-300/40  overflow-x-scroll overflow-y-hidden  scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-orange-300 items-center flex snap-x snap-mandatory h-[90%]">
           {projects.map((project, i) => (
             <div
-              key={project._id}
+              key={project?._id}
               className=" w-full h-full flex-shrink-0 snap-center flex flex-col items-center justify-center   0 p-5 lg:space-x-20 "
             >
               <div className="border-4 border-my-skyblue p-5 h-[99%] w-[90%] flex flex-col items-center lg:grid lg:grid-cols-10 lg:flex-row ">
@@ -52,7 +52,7 @@ export default function ProjectSection({ projects }: Props) {
                 >
                   <div className="rounded-xl bg-mine p-2">
                     <Image
-                      src={urlFor(project.image).url()}
+                      src={urlFor(project?.image).url()}
                       height={500}
                       width={700}
                       objectFit="contain"
@@ -64,18 +64,18 @@ export default function ProjectSection({ projects }: Props) {
                 <div className="max-w-[450px] lg:col-span-5 lg:mx-auto">
                   <h4 className="text-3xl md:text-4xl font-semibold text-center lg:space-y-10">
                     <span className=" underline decoration-yellow-300/50 ">
-                      Case {1 + i} of {projects.length}
+                      Case {1 + i} of {projects?.length}
                     </span>{" "}
                     <h4>{project?.title}</h4>
                   </h4>
                   <div className="flex items-center justify-center mt-4 mb-2 lg:mt-10 lg:mb-5 w-fit space-x-2 mx-auto">
                     {project?.technologies.map((technology) => (
-                      <div key={technology._id}>
+                      <div key={technology?._id}>
                         <Image
                           height={40}
                           width={40}
                           className="object-cover rounded-full"
-                          src={urlFor(technology.image).url()}
+                          src={urlFor(technology?.image).url()}
                         />
                       </div>
                     ))}
