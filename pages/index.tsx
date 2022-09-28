d8e55ec0b87b4c9dccb7776d53365529c02bf0fe
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-09-28 20:57:35 +0900

    amend getStaticProps 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { GetStaticProps, NextPage } from "next";
+import { groq } from "next-sanity";
 import Head from "next/head";
 import ContactSction from "../components/ContactSction";
 import Footer from "../components/Footer";
@@ -6,20 +7,20 @@ import Header from "../components/Header";
 import ProjectSection from "../components/ProjectSection";
 import TimelineSection from "../components/TimelineSection";
 import WelcomeSection from "../components/WelcomeSection";
+import { sanityClient } from "../sanity";
 import { Project, Skill } from "../typing";
 import { fetchProjects } from "../utils/fetchProjects";
 import { fetchSkills } from "../utils/fetchSkill";
 
 type Props = {
-  skills: Skill[],
-  projects: Project[],
-}
+  skills: Skill[];
+  projects: Project[];
+};
 
-
-const Home = ({projects, skills} : Props) => {
+const Home = ({ projects, skills }: Props) => {
   return (
     <div className=" h-screen text-my-black  scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 z-30">
-    {/* // <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"> */}
+      {/* // <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"> */}
       <Head>
         <title>Portfolio 2.0</title>
       </Head>
@@ -28,26 +29,24 @@ const Home = ({projects, skills} : Props) => {
       <Header />
 
       {/* ---Welcome Section--- */}
-      <WelcomeSection skills={skills}/>
+      <WelcomeSection skills={skills} />
 
       {/* Timeline Section */}
       <TimelineSection />
 
       {/* ---Project Section--- */}
-      <ProjectSection projects={projects}/>
+      <ProjectSection projects={projects} />
 
       {/* ---Contact Section--- */}
       <ContactSction />
-      
+
       {/* ---Footer Section--- */}
       <Footer />
-
     </div>
   );
 };
 
 export default Home;
-
 
 // export const getStaticProps:GetStaticProps = async (context) =>{
 //   const skills: Skill[] = await fetchSkills();
@@ -63,9 +62,21 @@ export default Home;
 // }
 
 // This function gets called at build time
+
+const skillsQuery = groq`
+  *[_type == "skill"]
+`;
+
+const projectsQuery = groq`
+  *[_type == "project"]{
+    ...,
+    technologies[]->
+  }
+`;
+
 export async function getStaticProps() {
-  const skills: Skill[] = await fetchSkills();
-  const projects: Project[] = await fetchProjects();
+  const skills: Skill[] = await sanityClient.fetch(skillsQuery);
+  const projects: Project[] = await sanityClient.fetch(projectsQuery);
 
   return {
     props: {
@@ -75,4 +86,3 @@ export async function getStaticProps() {
     revalidate: 10,
   };
 }
-
