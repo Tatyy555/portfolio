3e98a1aa610db377ed379f143305fcf134057b5c
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-10-04 00:14:43 +0900

    add wqeb3 game into timeline

--- components/TimelineSection.tsx
+++ components/TimelineSection.tsx
@@ -25,7 +25,23 @@ export default function TimelineSection({}: Props) {
       >
         <h1 className="mb-10 text-4xl underline font-extrabold">Timeline</h1>
         <ul className="p-10 shadow-xl shadow-blue-300/40 text-left leading-8  lg:text-xl lg:leading-loose bg-my-white sm:overflow-y-scroll sm:scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-orange-300">
-        <li>
+          <li>
+            <strong>Oct 2022</strong> -{" "}
+            <a
+              href="https://my-nft-game-xfn6.vercel.app/"
+              className="underline hover:text-my-yellow hover:duration-300"
+            >
+              &quot;Web3 Game&quot;
+            </a>{" "}
+            (JavaScript & Solidity) wwith reference to the study material of{" "}
+            <a
+              href="https://www.unchain.tech/"
+              className="underline hover:text-my-yellow hover:duration-300"
+            >
+              Unchain
+            </a>
+          </li>
+          <li>
             <strong>Spt 2022</strong> -{" "}
             <a
               href="https://basketball-scoreboard-dj8a.vercel.app/"
