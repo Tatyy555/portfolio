54bda98fd97ac5e633030b10d8d9f68725874260
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-11-10 12:23:07 +0900

    add NFT market

--- components/TimelineSection.tsx
+++ components/TimelineSection.tsx
@@ -26,6 +26,23 @@ export default function TimelineSection({}: Props) {
         <h1 className="mb-10 text-4xl underline font-extrabold">Timeline</h1>
         <ul className="p-10 shadow-xl shadow-blue-300/40 text-left leading-8  lg:text-xl lg:leading-loose bg-my-white sm:overflow-y-scroll sm:scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-orange-300">
           <li>
+            <strong>Nov 2022</strong> -{" "}
+            <a
+              href="https://ebay-clone-thirdweb-omega.vercel.app/"
+              className="underline hover:text-my-yellow hover:duration-300"
+            >
+              NFT Marketplace
+            </a>{" "}
+            with reference to{" "}
+            <a
+              href="https://www.youtube.com/watch?v=MegfLgs-UU8&list=PLf16UKl7nR5D06fAYUq8qy-BgwaWFpv3B"
+              className="underline hover:text-my-yellow hover:duration-300"
+            >
+              the YouTube by Sonny Sangha
+            </a>
+          </li>
+
+          <li>
             <strong>Oct 2022</strong> -{" "}
             <a
               href="https://www.freecodecamp.org/certification/TATYY/front-end-development-libraries"
